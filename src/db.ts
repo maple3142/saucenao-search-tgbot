a5ba77b9e712ad8302d95a5f569e3c98d03f3c4e
maple3142 <kirby741852963@gmail.com>
2020-09-12 22:14:04 +0800

    use knex instead

--- src/db.ts
+++ src/db.ts
@@ -1,53 +1,64 @@
 import { Client } from 'pg'
 import debug from 'debug'
+import Knex = require('knex')
 
 const log = debug('saucenao-tg:db')
 
-const opts: any = {
-	ssl: true
+const opts: Knex.Config = {}
+if (process.env.DB_TYPE && process.env.DB_URL) {
+	opts.client = process.env.DB_TYPE
+	opts.connection = process.env.DB_URL
 }
-if (process.env.DATABASE_URL) {
-	opts.connectionString = process.env.DATABASE_URL
-}
-const client = new Client(opts)
-
-client.connect()
+const db = Knex(opts)
 
 type UID = string | number
 export function isUserCreated(uid: UID) {
-	return client
-		.query('SELECT uid FROM userdata WHERE uid=$1', [uid.toString()])
-		.then(({ rows }) => rows.length > 0)
+	return db('userdata')
+		.select('uid')
+		.where({ uid: uid.toString() })
+		.then(rows => rows.length > 0)
 		.then(result => {
 			log(`isUserCreated uid=${uid} result=${result}`)
 			return result
 		})
 }
 export function createUser(uid: UID) {
-	return client
-		.query('INSERT INTO userdata (uid,min_similarity,max_result_count) VALUES ($1,$2,$3)', [
-			uid.toString(),
-			process.env.MINIUM_SIMILARITY,
-			process.env.MAX_RESULT_COUNT
-		])
+	return db('userdata')
+		.insert({
+			uid: uid.toString(),
+			min_similarity: process.env.MINIUM_SIMILARITY,
+			max_result_count: process.env.MAX_RESULT_COUNT
+		})
 		.then(() => {
 			log(`createUser uid=${uid}`)
 			return true
 		})
 }
-export function getUserData<T>(uid: UID, field: string, defaultValue: T): Promise<T> {
+export function getUserData<T>(
+	uid: UID,
+	field: string,
+	defaultValue: T
+): Promise<T> {
 	// field is guaranteed to come from trusted input
-	return client
-		.query(`SELECT ${field} FROM userdata WHERE uid=$1`, [uid.toString()])
-		.then(({ rows }) => rows[0][field] || defaultValue)
+	return db('userdata')
+		.select(field)
+		.where({ uid: uid.toString() })
+		.then(rows => rows[0][field] || defaultValue)
 		.then(result => {
 			log(`getUserData uid=${uid} field=${field} result=${result}`)
 			return result
 		})
 }
-export function setUserData<T>(uid: UID, field: string, value: T): Promise<boolean> {
-	return client.query(`UPDATE userdata SET ${field}=$2 WHERE uid=$1`, [uid.toString(), value]).then(() => {
-		log(`setUserData uid=${uid} field=${field} value=${value}`)
-		return true
-	})
+export function setUserData<T>(
+	uid: UID,
+	field: string,
+	value: T
+): Promise<boolean> {
+	return db('userdata')
+		.update({ [field]: value })
+		.where({ uid: uid.toString() })
+		.then(() => {
+			log(`setUserData uid=${uid} field=${field} value=${value}`)
+			return true
+		})
 }
